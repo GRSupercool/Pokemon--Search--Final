8c85a7791d175cc5df4948111985586efa64abf7
GRSupercool <ms.georgina.ramirez@gmail.com>
2019-05-18 18:09:57 -0700

    37

--- src/router.js
+++ src/router.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Rhymesaurus from '@/views/Rhymesaurus'
 // TODO: Import new component
-
+import AdjForNoun from '@/views/AdjfornNoun'
 Vue.use(Router)
 
 export default new Router({
@@ -11,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Rhymesaurus',
       component: Rhymesaurus
+    },
+    {
+      path: '/adjfornoun',
+      name: 'adjfornoun',
+      component: AdjForNoun 
     }
     // TODO: Add new route definition here.
   ]
